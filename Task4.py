ba9150d713f6fb5843e4b21a115fe20a48cde4e0
kozhevnikovanataly@yandex.ru <kozhevnikovanataly@yandex.ru>
2022-12-02 19:24:53 +0300

    commit

---
+++ Task4.py
@@ -0,0 +1,15 @@
+from random import randint
+
+def list(N):
+    list = []
+    for i in range(N):
+        list.append(randint(-N, N)
+    return list
+
+N = int(input('Введите число N: '))
+numbers = list(N)
+print(num)
+x = open('file.txt','r')
+result = numbers[int(x.readline())] * numbers[int(x.readline(2))]
+print(result)
+
